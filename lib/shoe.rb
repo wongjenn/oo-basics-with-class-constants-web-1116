fc30063e3eec5c3acf32cc92ade7c47d5f67d7f3
Jennifer Wong <wongjenn@yahoo.com>
2016-11-30 17:05:30 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,13 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader
+
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
   end
 
   def cobble
